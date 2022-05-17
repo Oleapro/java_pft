cb50e0074b353780e59c3cf90c5e92c2aa4088a4
Oleapro <olgaprocofieva@gmail.com>
2022-05-16 23:49:41 -0400

    Comparing List size
    List<GroupData> groups = new ArrayList<GroupData>();

--- addressbookWebTest/src/test/java/com/stqa/pft/addressbook/tests/DeleteGroupTests.java
+++ addressbookWebTest/src/test/java/com/stqa/pft/addressbook/tests/DeleteGroupTests.java
@@ -4,22 +4,28 @@ import com.stqa.pft.addressbook.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class DeleteGroupTests extends TestBase {
 
 
   @Test
   public void testDeleteGroup() throws Exception {
     app.getNavigationHelper().go_toGroupPage();
-    int before = app.getGroupHelper().getGroupCount();
     if (!app.getGroupHelper().isThereAGroup()){
       app.getGroupHelper().createGroup(new GroupData("test1", null, null));
     }
-    app.getGroupHelper().selectGroup();
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    //int before = app.getGroupHelper().getGroupCount();
+    app.getGroupHelper().selectGroup(before.size()-1);
     app.getGroupHelper().deleteSelectedGroup();
     app.getGroupHelper().returnToGroupPage();
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before - 1);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    //int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after.size(), before.size() - 1);
+
   }
+
 
 
 }
--- addressbookWebTest/src/test/java/com/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbookWebTest/src/test/java/com/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,16 +4,21 @@ import com.stqa.pft.addressbook.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class GroupCreationTests extends TestBase {
 
 
   @Test
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().go_toGroupPage();
-    int before = app.getGroupHelper().getGroupCount();
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    //int before = app.getGroupHelper().getGroupCount();
     app.getGroupHelper().createGroup(new GroupData("test1", null, null));
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after,before  + 1);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    //int after = app.getGroupHelper().getGroupCount();
+    //Assert.assertEquals(after,before  + 1);
+    Assert.assertEquals(after.size(), before.size() +1);
 
   }
 
